44129b15ae1cea9ea4d82522d9a67e7ee4f9a1db
Wei Huang <grubbyfans@gmail.com>
2018-11-27 21:03:50 +0100

    Build release with distillery

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,15 @@
 KEY_FILE=/tmp/travis_rsa
+BUILD=_build/prod/rel/sbanken_monitor/
+DEST_FOLDER=sbanken_monitor
 
-echo Key $KEY_FILE
-echo Target $DEPLOY_TARGET
-which ssh
-ssh -oStrictHostKeyChecking=no -i $KEY_FILE $DEPLOY_TARGET pwd
+function ssh_command {
+    cmd=$1
+    ssh -oStrictHostKeyChecking=no -i $KEY_FILE $DEPLOY_TARGET $cmd
+}
+
+ssh_command mkdir -p $DEST_FOLDER
+ssh_command $DEST_FOLDER/bin/sbanken_monitor stop
+scp -r $BUILD $DEPLOY_TARGET:~/
+ssh_command ls $DEST_FOLDER
+
+
