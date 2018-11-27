4d00b93815690a8e01350cf7d31f3c14b239befd
Wei Huang <grubbyfans@gmail.com>
2018-11-27 21:08:07 +0100

    deploy script tweak

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,13 @@ BUILD=_build/prod/rel/sbanken_monitor/
 DEST_FOLDER=sbanken_monitor
 
 function ssh_command {
-    cmd=$1
+    cmd="$@"
     ssh -oStrictHostKeyChecking=no -i $KEY_FILE $DEPLOY_TARGET $cmd
 }
 
 ssh_command mkdir -p $DEST_FOLDER
 ssh_command $DEST_FOLDER/bin/sbanken_monitor stop
-scp -r $BUILD $DEPLOY_TARGET:~/
+scp -oStrictHostKeyChecking=no -i $KEY_FILE -r $BUILD $DEPLOY_TARGET:~/
 ssh_command ls $DEST_FOLDER
 
 
