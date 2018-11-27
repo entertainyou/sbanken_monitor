40e227d623d3c1a742da4f8aa2c69b2a9c1492b1
Wei Huang <grubbyfans@gmail.com>
2018-11-27 20:55:25 +0100

    Dont chek host key for ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ KEY_FILE=/tmp/travis_rsa
 echo Key $KEY_FILE
 echo Target $DEPLOY_TARGET
 which ssh
-ssh -i $KEY_FILE $DEPLOY_TARGET pwd
+ssh -oStrictHostKeyChecking=no -i $KEY_FILE $DEPLOY_TARGET pwd
