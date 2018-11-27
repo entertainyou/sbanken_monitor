f173f3784a0ccdfaee618d9b2a1de97b3997da75
Wei Huang <grubbyfans@gmail.com>
2018-11-27 21:31:15 +0100

    More deploy script tweak

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,9 +9,9 @@ function ssh_command {
 
 ssh_command mkdir -p $DEST_FOLDER
 ssh_command $DEST_FOLDER/bin/sbanken_monitor stop
-ssh_command rm -rf $DEST_FOLDER
+ssh_command rm -rf $DEST_FOLDER/*
 scp -q -oStrictHostKeyChecking=no -i $KEY_FILE -r $BUILD $DEPLOY_TARGET:~/ &&
     ssh_command tar -zxvf ~/sbanken_monitor.tar.gz -C $DEST_FOLDER &&
-    ssh_command $DEST_FOLDER/bin/sbanken_monitor star
+    ssh_command LANG=en_US.utf8 $DEST_FOLDER/bin/sbanken_monitor start
 
 
