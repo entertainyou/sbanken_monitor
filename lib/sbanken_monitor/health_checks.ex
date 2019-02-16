23e4dda61b50d4af682285f9b30de8876ef86e10
Wei Huang <grubbyfans@gmail.com>
2019-02-16 22:41:41 +0100

    Add missing health_checks file

---
+++ lib/sbanken_monitor/health_checks.ex
@@ -0,0 +1,16 @@
+defmodule SbankenMonitor.HealthChecks do
+  alias Timex.Duration 
+  @interval 1 |> Duration.from_minutes() |> Duration.to_milliseconds(truncate: true)
+  @heartbeat_url Application.get_env(:sbanken_monitor, :heartbeat_url)
+
+  defmodule State do
+    defstruct foo: nil
+  end
+
+  use SbankenMonitor.Agent, interval: @interval, immediate: true
+
+  def do_work(state) do
+    Tesla.get(@heartbeat_url)
+    state
+  end
+end
